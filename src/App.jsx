0712cc571a58e704ca229fc9e9d09d23384c147a
rdkhub <kv92xwtfjf@privaterelay.appleid.com>
2026-06-20 20:59:19 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -388,7 +388,7 @@ function SelectCard({ active, onClick, text, compact, icon }) {
 // =====================================================================
 // PIN LOCK
 // =====================================================================
-const ADMIN_PIN = "1234"; // cámbialo aquí por el PIN que quieras usar en el local
+const ADMIN_PIN = "5290"; // cámbialo aquí por el PIN que quieras usar en el local
 
 function PinScreen({ onUnlock, onBack }) {
   const [pin, setPin] = useState("");
